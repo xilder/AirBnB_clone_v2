fdc50b150f9fed3f0e93e366e59c5b3a9f8ce11b
xilder <olabowaleayobami@gmail.com>
2023-11-20 23:45:39 -0800

    updates models/city.py for compatibility with databases

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.state import State
+from models.place import Place
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+#    places = relationship('Places', backref='cities', cascade='all, delete-orphan'))
+
+    def __init__(self, *args, **kwargs):
+        """Initialises city"""
+        super().__init__(*args, **kwargs)
