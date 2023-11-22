516f31a9f2a2927496d5fbece03af4b88ae043a0
xilder <olabowaleayobami@gmail.com>
2023-11-22 01:05:22 -0800

    update the do_all function of the console to print properly

--- console.py
+++ console.py
@@ -241,7 +241,7 @@ class HBNBCommand(cmd.Cmd):
             for k, v in storage.all().items():
                 print_list.append(str(v))
 
-        print(print_list)
+        print("[" + ", ".join(print_list) + "]")
 
     def help_all(self):
         """ Help information for the all command """
