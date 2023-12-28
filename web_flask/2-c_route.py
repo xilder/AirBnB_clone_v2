0cf76f194695f7f1d62511172c1bb18f992ae369
xilder <olabowaleayobami@gmail.com>
2023-12-28 05:29:01 -0800

    adds 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+a flask web application listening based on 0-hbnb_route.py that prints
+'HBNB' using /hbnb path
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns a given string"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns a given string"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Returns a given string"""
+    text = text.replace('_', ' ')
+    return (f"C {text}")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
