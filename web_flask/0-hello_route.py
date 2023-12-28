1836476ead406baf3c6b8d99b8cc5c9a2716f366
xilder <olabowaleayobami@gmail.com>
2023-12-28 04:43:32 -0800

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_world():
     """Returns a given string"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
